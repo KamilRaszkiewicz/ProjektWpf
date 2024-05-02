0e061ae9b91ff4bd6a9278af4923c296ab931bda
roska <r.oskar.15@wp.pl>
2024-05-02 21:04:53 +0200

    Crud Works

--- TestsGenerator.WPF/Views/Pages/DashboardPage.xaml.cs
+++ TestsGenerator.WPF/Views/Pages/DashboardPage.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.Web.WebView2.Core;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Windows.Controls;
 using TestsGenerator.WPF.ViewModels.Pages;
 using Wpf.Ui.Controls;
@@ -8,28 +10,44 @@ namespace TestsGenerator.WPF.Views.Pages
     public partial class DashboardPage : INavigableView<DashboardViewModel>
     {
         public DashboardViewModel ViewModel { get; }
+        private ObservableCollection<Answers> dataCollection = new ObservableCollection<Answers>();
+
 
         public DashboardPage(DashboardViewModel viewModel)
         {
             ViewModel = viewModel;
-            DataContext = this;
-
             InitializeComponent();
+            Answers_Grid.ItemsSource = dataCollection;
+            Change_Width();
         }
 
-        private void TextBox_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        private async Task Change_Width()
         {
+            await Task.Delay(1000);
+            Answers_Grid.Columns[0].Width = new DataGridLength(730);
 
         }
-
-        private void DataGrid_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
-        {
-
-        }
-
         private void Add_Answer(object sender, RoutedEventArgs e)
         {
-            Answers_Grid.Items.Add(new {}) ;
+            dataCollection.Add(new Answers());
+            Answers_Grid.Columns[0].Width = new DataGridLength(730);
         }
+
+        private void Delete_Answer(object sender, RoutedEventArgs e)
+        {
+            if (Answers_Grid.SelectedItem != null)
+            {
+                Answers selectedData = (Answers)Answers_Grid.SelectedItem;
+                dataCollection.Remove(selectedData);
+            }
+        }
+    }
+
+    public class Answers
+    {
+        public string Odpowiedź { get; set; }
+        public bool Prawidłowa { get; set; }
+        
+        
     }
 }
